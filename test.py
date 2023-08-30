9749a677f10b83a2acfe108b8fd9594ac5a90226
natemcc30 <natemcc30@gmail.com>
2023-08-30 16:30:21 -0700

    basic flask working

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
-@app.route("/home")
-def home():
-    return "hello world flask"
+
+@app.route("/home/<num>")
+def home(num):
+    numb = int(num)
+    if (numb % 2) == 0:
+        numb = True
+    else:
+        numb = False
+    return str(numb)
+
 
 if __name__ == "__main__":
-    # Please do not set debug=True in production
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=True)
