3eaa051ba61f75904fac47f7aecb32663b7b0085
natemcc30 <natemcc30@gmail.com>
2023-09-13 18:31:49 -0700

    session storage functional

--- meme.py
+++ meme.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, send_from_directory, url_for
+from flask import Flask, render_template, send_from_directory, url_for, request, session
 import json
 import requests
 
 app = Flask(__name__)
-
+app.secret_key = 'your_secret_key_here'
 
 class Meme:
 
@@ -21,7 +21,6 @@ class Meme:
 
 meme = Meme()
 
-
 # @app.route('/favicon.ico')
 # def favicon():
 #     return send_from_directory(os.path.join(app.root_path, 'static'),
@@ -36,20 +35,19 @@ def idx():
     return render_template("meme.html", **meme.get())
 
 
+# for addidng the liked images to our session
+@app.route("/like_image", methods=['POST'])
+def like_images():
+    image_url = request.form.get('url')
+    session['url'] = image_url
+    return 'Image stored in session'
+
+
 @app.route("/likes")
 def likes():
-    return render_template('likes.html')
+    return render_template("likes.html")
 
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8080, debug=True)
 
-
-
-# def get_meme():
-#     url = "https://meme-api.com/gimme"
-#     response = json.loads(requests.request("GET", url).text)
-#     meme_large = response["preview"][-2]
-#     subreddit = response["subreddit"]
-#     return meme_large, subreddit
-#     <a href=”/your_flask_funtion”><input type=”button” value=”call_flask_funtion”></a>
--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -9,4 +9,3 @@ window.onload = function () {
     });
 
 }
-
